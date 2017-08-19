1d2ad334db6e7392584cc30ccfabfefb8585a7c4
damindaeshan <damindaeshan@gmail.com>
2017-08-19 18:52:36 +1000

    Developmenet of UsageRecord Cons and getSet

--- src/bcccp/tickets/season/UsageRecord.java
+++ src/bcccp/tickets/season/UsageRecord.java
@@ -1,47 +1,42 @@
 package bcccp.tickets.season;
 
+// Usage record class that used to implement Usage record interface 
 public class UsageRecord implements IUsageRecord {
 	
 	String ticketId;
 	long startDateTime;
 	long endDateTime;
 	
-	
-	
+	//Constructor for Usage Record class
 	public UsageRecord(String ticketId, long startDateTime) {
-		//TODO Implement constructor
+		this.ticketId = ticketId;
+		this.startDateTime = startDateTime;
 	}
 
-
-
+	//
 	@Override
 	public void finalise(long endDateTime) {
 		// TODO Auto-generated method stub
 		
 	}
 
-
-
+	//Method used for access start time
 	@Override
 	public long getStartTime() {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		return this.startDateTime;
 	}
 
-
-
+	//Method used for access end date time
 	@Override
 	public long getEndTime() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.getEndTime();
 	}
 
-
-
+	//Method used for access season ticket id
 	@Override
 	public String getSeasonTicketId() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.ticketId;
 	}
 	
 	
