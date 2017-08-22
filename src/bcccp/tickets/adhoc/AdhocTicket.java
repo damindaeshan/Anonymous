42e43f7d7ca6d905e89634639041d69090f3826a
Pradeep Lakshitha <jpradeeplakshitha@gmail.com>
2017-08-22 13:48:21 +1000

    AdhocTicket.java - getCarparkId() method
    I have implemented the getCarparkId() method

--- src/bcccp/tickets/adhoc/AdhocTicket.java
+++ src/bcccp/tickets/adhoc/AdhocTicket.java
@@ -35,8 +35,7 @@ public class AdhocTicket implements IAdhocTicket {
 
 	@Override
 	public String getCarparkId() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.carparkId;
 	}
 
 
