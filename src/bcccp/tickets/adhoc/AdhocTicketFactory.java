3d6cc77dc3a02ed676c90320123fd8e0f5b60c72
Pradeep Lakshitha <jpradeeplakshitha@gmail.com>
2017-08-22 15:26:00 +1000

    AdhockTicketFactory.java - add comments
    I have added code comments to the make() method

--- src/bcccp/tickets/adhoc/AdhocTicketFactory.java
+++ src/bcccp/tickets/adhoc/AdhocTicketFactory.java
@@ -5,16 +5,24 @@ public class AdhocTicketFactory implements IAdhocTicketFactory {
 	@Override
 	public IAdhocTicket make(String carparkId, int ticketNo) {
 		
+		//This Statement will create a pattern of Date and Time
 		SimpleDateFormat simple_date_format = new SimpleDateFormat("dd:MM:YYYY HH:mm:ss");
         
+		//This Statement will get the current milliseconds of date and time
         	long system_time = System.currentTimeMillis();
         
+		//This Satement will create a barcode by including ticketNo, carparkId and the date and time
         	String create_barcode = Integer.toString(ticketNo) + "/" + carparkId + "/" + simple_date_format.format(system_time);
         
+		/*This Statement will call to the overrided constructor of AdhockTicket by parsing
+		the carparkId, ticketNo and create_barcode through the argument list
+		*/
         	AdhocTicket create_new_ticket = new AdhocTicket(carparkId, ticketNo, create_barcode);
         
+		//This Statement will set the time, when entering to the car park
         	create_new_ticket.enter(system_time);
         
+		//This Statement will return the new ticket
         	return create_new_ticket;
 	}
 
