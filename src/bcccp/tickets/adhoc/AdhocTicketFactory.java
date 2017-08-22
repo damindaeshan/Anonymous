9d6c63fb8b511d9832c368cd1da9fd30a962887d
Pradeep Lakshitha <jpradeeplakshitha@gmail.com>
2017-08-22 15:17:12 +1000

    AdhocTicketFactory.java - make() method
    I have implemented the make() method of AdhocTicketFactory.java class

--- src/bcccp/tickets/adhoc/AdhocTicketFactory.java
+++ src/bcccp/tickets/adhoc/AdhocTicketFactory.java
@@ -4,8 +4,18 @@ public class AdhocTicketFactory implements IAdhocTicketFactory {
 
 	@Override
 	public IAdhocTicket make(String carparkId, int ticketNo) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		SimpleDateFormat simple_date_format = new SimpleDateFormat("dd:MM:YYYY HH:mm:ss");
+        
+        	long system_time = System.currentTimeMillis();
+        
+        	String create_barcode = Integer.toString(ticketNo) + "/" + carparkId + "/" + simple_date_format.format(system_time);
+        
+        	AdhocTicket create_new_ticket = new AdhocTicket(carparkId, ticketNo, create_barcode);
+        
+        	create_new_ticket.enter(system_time);
+        
+        	return create_new_ticket;
 	}
 
 
